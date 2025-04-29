9efd90b2bbfecdcf7273ead649f16827df6275dd
Dmytro Davidenko <dima080885@gmail.com>
2025-04-29 16:13:18 +0300

    add swager in production

--- TouristBookingPlatform.API/Program.cs
+++ TouristBookingPlatform.API/Program.cs
@@ -16,11 +16,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tourist Booking API V1");
+});
 
 app.UseHttpsRedirection();
 
